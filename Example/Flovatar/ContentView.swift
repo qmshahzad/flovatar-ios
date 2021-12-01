8df0318800b97f45cc822196b6b29f439fed9c7a
Yuriy Berdnikov <yuriy@mac.local>
2021-12-01 20:27:36 +0200

    Minor code cleanup

--- Example/Flovatar/ContentView.swift
+++ Example/Flovatar/ContentView.swift
@@ -20,31 +20,36 @@ import AVKit
 import SwiftUI
 
 struct ContentView: View {
-    @ObservedObject var viewModel = ViewModel()
+    @StateObject var viewModel = ViewModel()
     @State private var isShowingDetailView = false
 
     let backgroundGradient = LinearGradient(gradient: Gradient(colors: [Color(red: 1.00, green: 0.00, blue: 0.98), Color(red: 0.26, green: 0.11, blue: 0.56)]), startPoint: .top, endPoint: .bottom)
     
     var body: some View {
-        
         NavigationView {
             ZStack {
                 backgroundGradient
                     .ignoresSafeArea()
+
                 VStack {
-                    Image("flovatar-logo").padding(.bottom)
+                    Image("flovatar-logo")
+                        .padding(.bottom)
+                    
                     Button("Login with Blocto") {
                         viewModel.authn(provider: .blocto)
                     }
                     .padding()
                     .background(Color(red: 0.26, green: 0.11, blue: 0.56))
                     .clipShape(Capsule())
-                    NavigationLink(destination: DetailView().navigationBarBackButtonHidden(true), isActive: $viewModel.advance) { EmptyView() }
+
+                    NavigationLink(
+                        destination: DetailView(),
+                        isActive: $viewModel.advance
+                    ) { EmptyView() }
                 }
                 .accentColor(Color.white)
             }
 
         }
-        
     }
 }
